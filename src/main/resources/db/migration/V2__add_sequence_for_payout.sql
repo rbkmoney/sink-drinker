5b179486c1583bc1d0586bf7ff3a94cbe8083833
karleowne <karleowne@gmail.com>
2021-06-30 14:59:42 +0700

    add V2 migration

---
+++ src/main/resources/db/migration/V2__add_sequence_for_payout.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS sink_drinker.sequence_for_payout
+(
+    payout_id   CHARACTER VARYING NOT NULL,
+    sequence_id INTEGER           NOT NULL,
+    CONSTRAINT sequence_for_payout_pkey PRIMARY KEY (payout_id)
+);
